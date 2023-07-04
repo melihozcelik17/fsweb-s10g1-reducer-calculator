e3afdc391f93cbc4d6778ac6084d5977a5470d58
melihozcelik17 <melih.ozcelik17@gmail.com>
2023-07-04 11:02:07 +0300

    s10 d1 projesi

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,11 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+
+export const ADD_MEMORY = "ADD_MEMORY";
+export const APPLY_RECALL_MEMORY = "APPLY_RECALL_MEMORY"
+export const CLEAR_MEMORY = "CLEAR_MEMORY"
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
@@ -10,3 +15,25 @@ export const addOne = () => {
 export const applyNumber = (number) => {
   return ({ type: APPLY_NUMBER, payload: number });
 }
+
+export const changeOperation = (operator) => {
+  return ({ type: changeOperation, payload: operator });
+}
+
+export const clearDisplay = () => {
+  return ({ type: CLEAR_DISPLAY });
+}
+
+export const addMemory = () => {
+  return ({ type: ADD_MEMORY });
+}
+
+export const applyRecallMemory = () => {
+  return ({ type: APPLY_RECALL_MEMORY });
+}
+
+export const clearMemory = () => {
+  return ({ type: CLEAR_MEMORY });
+}
+
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,18 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import {
+  ADD_ONE,
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  ADD_MEMORY,
+  APPLY_RECALL_MEMORY,
+  CLEAR_MEMORY,
+
+} from './../actions';
 
 export const initialState = {
-  total: 100,
-  operation: "*",
-  memory: 100
+  total: 0,
+  operation: "+",
+  memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -38,6 +47,28 @@ const reducer = (state, action) => {
         ...state,
         operation: action.payload
       });
+    case (CLEAR_DISPLAY):
+      return ({
+        ...state,
+        total: 0
+      });
+
+    case (ADD_MEMORY):
+      return ({
+        ...state,
+        memory: state.total
+      });
+    case (APPLY_RECALL_MEMORY):
+      return ({
+        ...state,
+        total: calculateResult(state.total, action.payload, state.operation)
+      });
+    case (CLEAR_MEMORY):
+      return ({
+        ...state,
+        memory: initialState.memory
+      });
+
 
     default:
       return state;
